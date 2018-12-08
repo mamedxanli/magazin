64c1b78e2c08f0279dd89736136c3aa4326fe779
Alek Mammadov <mammadov.alek@gmail.com>
2018-12-08 12:12:04 -0500

    commiting changes

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Start Gunicorn processes
+echo Collecting static files
+python manage.py collectstatic --no-input
 echo Starting Gunicorn.
 exec gunicorn inetrecomgr.wsgi:application \
     --bind 0.0.0.0:8008 \
+    --log-level=debug \
+    #--access-logfile /var/log/gunicorn-access.log \
+    #--error-logfile /var/log/gunicorn-error.log \
     --workers 3
